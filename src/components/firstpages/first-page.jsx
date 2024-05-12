b77ec6d9d2968b3e424d09f3325b580bf37a9394
haikun66 <85195787+haikun66@users.noreply.github.com>
2024-05-12 14:10:55 +0200

    ADD PAGE

---
+++ src/components/firstpages/first-page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../firstpages/firstpages.css";
+function FirstPage() {
+    return (
+        <div className='firstAnimation'> 
+            <div >
+                <div className='image'></div>
+            </div>
+        </div>
+    );
+  }
+  
+  export default FirstPage;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/acceuil/home';
+import FirstPage from './components/firstpages/first-page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-    <App />
+    <FirstPage />
+    
     <Home />
+    
   </React.StrictMode>
 );
 
