e9edba224d473ece1ead0cfcf497aa387cac2aee
haikun66 <85195787+haikun66@users.noreply.github.com>
2024-05-13 13:55:35 +0200

    update 1

--- src/components/firstpages/first-page.jsx
+++ src/components/firstpages/first-page.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import "../firstpages/firstpages.css";
+const pdpimg = require('./pdpstyler.png');
 function FirstPage() {
     return (
         <div className='firstAnimation'> 
-            <div >
-                <div className='image'></div>
+            <div className='contenu'>
+                <div className='intro'>
+                    <h1 className='text'>Mon Portfolio</h1>
+                    <h2  className='text'>À propos de moi</h2>
+                    <p  className='text'>Je suis un développeur passionné par la création de logiciels innovants.</p>
+                    <p  className='text'>Explorez mes projets ci-dessous ou contactez-moi pour en savoir plus.</p>
+                </div>
+                
+                <img className='image' src={pdpimg} alt="pdp" />
             </div>
         </div>
     );
